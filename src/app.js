35c8232516d9d11a7fa8e4d9d01659fac5092a3b
Leonardo Lacerda <leonardo_lacerda@carrefour.com>
2023-03-07 13:30:22 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const app = require('express')();
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer, {
+  cors: {origin : '*'}
+});
+
+const port = 3000;
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('message', (message) => {
+    console.log(message);
+    io.emit('message', message);
+  });
+});
+
+httpServer.listen(port, () => console.log(`listening on port ${port}`));
